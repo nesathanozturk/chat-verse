bfc0f10d1d425da741a9134aa2fa56ce5dfdaf79
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-08 21:32:19 +0300

    feat: added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/navbar";
+
 import Login from "./pages/login";
 import Chatroom from "./pages/chatroom";
 
@@ -6,8 +9,10 @@ const App = () => {
   return (
     <>
       <Navbar />
-      {/* <Login /> */}
-      <Chatroom />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chatroom" element={<Chatroom />} />
+      </Routes>
     </>
   );
 };
