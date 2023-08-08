0c06f2bd20231216573bbcfee04003f4a1fdc276
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-08 20:21:44 +0300

    feat: created header

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,8 +1,11 @@
 const Navbar = () => {
   return (
-    <nav className="navbar bg-primary text-primary-content">
-      <a className="btn btn-ghost normal-case text-xl">Chatverse</a>
-    </nav>
+    <header className="navbar bg-primary text-primary-content">
+      <nav className="containerWrapper flex justify-between">
+        <a className="btn btn-ghost normal-case text-xl">Chatverse</a>
+        <button>Log out</button>
+      </nav>
+    </header>
   );
 };
 
