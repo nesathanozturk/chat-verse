fcc041f37e7d79ca9014d5bbf633f042a09d4a52
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-08 20:12:25 +0300

    style: edited login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,20 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">
+            Embark on a Seamless Conversational Journey 🚀
+          </h1>
+          <p className="py-6 text-xl font-semibold">
+            Elevate your chat experience and connect like never before. Start
+            chatting today and let your words create magic!
+          </p>
+          <button className="btn btn-primary">Let&apos;s chat!</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
