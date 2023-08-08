2dc271051e43a76bf5694da4bc5f152d9f789780
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-08 21:39:25 +0300

    feat: created private route

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import Navbar from "./components/navbar";
 import Login from "./pages/login";
 import Chatroom from "./pages/chatroom";
 
+import PrivateRoute from "./routes/PrivateRoute";
+
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/chatroom" element={<Chatroom />} />
+        <Route
+          path="/chatroom"
+          element={
+            <PrivateRoute>
+              <Chatroom />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
