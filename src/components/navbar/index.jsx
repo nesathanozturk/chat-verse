6390481acada2c9166fa232df30f33bafec49c15
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-09 11:58:56 +0300

    feat: created button variable

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,17 +3,19 @@ import useAuthContext from "../../hooks/use-auth-context";
 const Navbar = () => {
   const { currentUser, handleSignOut } = useAuthContext();
 
+  const isButtonHidden = currentUser ? (
+    <button onClick={handleSignOut} className="btn btn-neutral">
+      Log out
+    </button>
+  ) : (
+    ""
+  );
+
   return (
-    <header className="navbar bg-primary text-primary-content">
+    <header className="navbar fixed z-10 bg-primary text-primary-content">
       <nav className="containerWrapper flex justify-between">
         <a className="btn btn-ghost normal-case text-xl">Chatverse</a>
-        {currentUser ? (
-          <button onClick={handleSignOut} className="btn btn-neutral">
-            Log out
-          </button>
-        ) : (
-          ""
-        )}
+        {isButtonHidden}
       </nav>
     </header>
   );
