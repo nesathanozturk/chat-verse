a720ea9badd7e6d949753f5cdb3c8d1145944698
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-09 11:29:31 +0300

    feat: import data

--- src/components/chat-box/index.jsx
+++ src/components/chat-box/index.jsx
@@ -1,18 +1,26 @@
+import { useEffect, useState } from "react";
+import { collection, query, onSnapshot } from "firebase/firestore";
+
 import Message from "../message";
 
+import { db } from "../../config/firebase";
+
 const Chatbox = () => {
-  const messages = [
-    {
-      id: 1,
-      name: "Nesathan",
-      text: "Hello there!",
-    },
-    {
-      id: 2,
-      name: "Mark",
-      text: "Hi!",
-    },
-  ];
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "messages"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+
+    return () => unsubscribe;
+  }, []);
 
   return (
     <div className="containerWrapper pb-44 pt-20">
