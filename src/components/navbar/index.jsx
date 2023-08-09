cd0ed0d64ec6a89d2b3a68b96cfc0cfefd171977
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-09 11:05:07 +0300

    feat: import current user and sign out func

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,19 @@
+import useAuthContext from "../../hooks/use-auth-context";
+
 const Navbar = () => {
+  const { currentUser, handleSignOut } = useAuthContext();
+
   return (
     <header className="navbar bg-primary text-primary-content">
       <nav className="containerWrapper flex justify-between">
         <a className="btn btn-ghost normal-case text-xl">Chatverse</a>
-        <button className="btn btn-neutral">Log out</button>
+        {currentUser ? (
+          <button onClick={handleSignOut} className="btn btn-neutral">
+            Log out
+          </button>
+        ) : (
+          ""
+        )}
       </nav>
     </header>
   );
