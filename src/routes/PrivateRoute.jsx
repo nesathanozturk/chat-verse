ca8da59f554bc1a551ffc1fe633dee159cf80d33
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-09 10:57:06 +0300

    feat: import currentuser

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,7 +1,9 @@
 import { Navigate } from "react-router-dom";
 
+import useAuthContext from "../hooks/use-auth-context";
+
 const PrivateRoute = ({ children }) => {
-  const currentUser = false;
+  const { currentUser } = useAuthContext();
 
   if (!currentUser) {
     return <Navigate to="/" replace={true} />;
