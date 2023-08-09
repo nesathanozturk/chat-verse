b8ad4c9e775becafbfc11290b6576db0056efdbd
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-08-09 10:31:25 +0300

    feat: added firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfmcuJdwgrMEmfMnMX7uD8omacCZBYG1s",
+  authDomain: "chatverse-dda3f.firebaseapp.com",
+  projectId: "chatverse-dda3f",
+  storageBucket: "chatverse-dda3f.appspot.com",
+  messagingSenderId: "1042512308442",
+  appId: "1:1042512308442:web:d7a5fc0ae7541ddc6d9de3",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
